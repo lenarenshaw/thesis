84b56c42c827d69d512691639b1265467695d4a6
Lena Renshaw <lena.renshaw@gmail.com>
2019-11-08 11:06:08 -0500

    Accessing mongo objects finally works

--- testdatabase.py
+++ testdatabase.py
@@ -3,22 +3,15 @@ import json
 import pandas as pd
 import sys, getopt, pprint
 from pymongo import MongoClient
-import os
-uri = os.environ['DB']
-
-def get_all_case_nums(db_cases):
-    cases = db_cases.find()
-    case_set = set()
-    j = 0
-    for j, case in enumerate(cases):
-        case_set.add(case["CaseNumber"])
-    return case_set
 
 def main():
-    client = MongoClient(uri)
-    db = client.get_database()
-    db_cases = db['cases']
-    cases = db_cases.find()
+    client = MongoClient('mongodb+srv://lenarenshaw:qwerty12@renshawthesis2-spgqz.mongodb.net/test?retryWrites=true&w=majority')
+
+    test_db = client['test_db']
+    test_collection = test_db['test_collection']
+    lst = test_collection.find()
+    for i in lst:
+        print(i)
 
 if __name__ == "__main__":
     main()

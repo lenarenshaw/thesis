da6b65234e85d5b949d5aa5e9414a105a64c45c2
Lena Renshaw <lena.renshaw@gmail.com>
2020-02-10 23:24:06 -0500

    Added oconnor new hampshire predictions

--- oconnor/calculate_sentiment.py
+++ oconnor/calculate_sentiment.py
@@ -1,43 +1,51 @@
-CONSIDER_LENGTH = True
-keywords = ["bennet", "biden", "bloomberg", "buttigieg", "gabbard", "klobuchar", "patrick", "sanders", "steyer", "warren", "yang", "democrat", "dem", "caucus", "primary"]
+state = 'newhampshire'
+keywords = ['bennet', 'biden', 'bloomberg', 'buttigieg', 'gabbard', 'klobuchar', 'patrick', 'sanders', 'steyer', 'warren', 'yang', 'democrat', 'dem', 'caucus', 'primary']
 
 all_scores = {}
+all_scores_length = {}
 for topic in keywords:
     all_scores[topic] = 0
+    all_scores_length[topic] = 0
 
 for topic in keywords:
-    file = open("../data/iowa/oconnor/" + topic + ".txt")
+    file = open('../data/' + state + '/oconnor/' + topic + '.txt')
     l = 0
     for line in file:
         l += 1
-    file = open("../data/iowa/oconnor/" + topic + ".txt_auto_anns/subjcluesSentenceClassifiersOpinionFinderJune06", "r")
+    file = open('../data/' + state + '/oconnor/' + topic + '.txt_auto_anns/subjcluesSentenceClassifiersOpinionFinderJune06', 'r')
     pos = 0
     neg = 0
     for line in file:
-        arr = line.split(" ")
-        type = arr[-1].split("\"")[1]
-        if type == "strongpos":
+        arr = line.split(' ')
+        type = arr[-1].split('\"')[1]
+        if type == 'strongpos':
             pos += 2
-        elif type == "weakpos":
+        elif type == 'weakpos':
             pos += 1
-        elif type == "strongneg":
+        elif type == 'strongneg':
             neg += 2
-        elif type == "weakneg":
+        elif type == 'weakneg':
             neg += 1
     if pos == 0 and neg == 0:
         score = 0
     elif neg == 0:
-        score = float("inf")
+        score = float('inf')
     else:
         score = (pos/neg)
-        if CONSIDER_LENGTH:
-            score = score * l
-    # print(topic + ": " + str(pos) + "/" + str(neg) + "*" + str(l) + " = " + str(score))
+    # print(topic + ': ' + str(pos) + '/' + str(neg) + '*' + str(l) + ' = ' + str(score))
     all_scores[topic] = score
+    all_scores_length[topic] = score * l
 
-outF = open("iowa_results_weighted.txt", "w")
+outF = open(state + '_results.txt', 'w')
 scores_sorted = sorted(all_scores.items(), key=lambda x: x[1], reverse=True)
 for (topic, score) in scores_sorted:
-    outF.write(topic + " sentiment: " + str(score))
-    outF.write("\n")
+    outF.write(topic + ' sentiment: ' + str(score))
+    outF.write('\n')
+outF.close()
+
+outF = open(state + '_results_weighted.txt', 'w')
+scores_length_sorted = sorted(all_scores_length.items(), key=lambda x: x[1], reverse=True)
+for (topic, score) in scores_length_sorted:
+    outF.write(topic + ' sentiment: ' + str(score))
+    outF.write('\n')
 outF.close()
--- oconnor/json_to_textfiles.py
+++ oconnor/json_to_textfiles.py
@@ -1,18 +1,21 @@
 import json
 
-keywords = ["bennet", "biden", "bloomberg", "buttigieg", "gabbard", "klobuchar", "patrick", "sanders", "steyer", "warren", "yang", "democrat", "dem", "caucus", "primary"]
+# state = 'iowa'
+state = 'newhampshire'
+
+keywords = ['bennet', 'biden', 'bloomberg', 'buttigieg', 'gabbard', 'klobuchar', 'patrick', 'sanders', 'steyer', 'warren', 'yang', 'democrat', 'dem', 'caucus', 'primary']
 
 all_tweets = {}
 for topic in keywords:
     all_tweets[topic] = []
 
-with open('../data/iowa/iowa.json') as json_file:
+with open('../data/' + state + '/'+ state +'.json') as json_file:
     data = json.load(json_file)
     for tweet in data:
-        text = tweet["text"]
-        text = text.replace("\n", " ")
-        text = text.replace(".", "")
-        text = text + "."
+        text = tweet['text']
+        text = text.replace('\n', ' ')
+        text = text.replace('.', '')
+        text = text + '.'
         for topic in keywords:
             if topic in text:
                 lst = all_tweets[topic]
@@ -20,8 +23,8 @@ with open('../data/iowa/iowa.json') as json_file:
                 all_tweets[topic] = lst
 
 for topic in keywords:
-    outF = open("../data/iowa/oconnor/" + topic + ".txt", "w")
+    outF = open('../data/' + state + '/oconnor/' + topic + '.txt', 'w')
     for line in all_tweets[topic]:
         outF.write(line)
-        outF.write("\n")
+        outF.write('\n')
     outF.close()
